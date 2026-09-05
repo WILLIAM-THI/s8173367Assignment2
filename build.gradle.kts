6bdd9c4ca20874a76300626d167ccc0dd33a51ed
William Thi <140167371+WILLIAM-THI@users.noreply.github.com>
2026-09-05 21:42:37 +1000

    Updated plugins build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+
+    id("com.google.devtools.ksp") version "2.0.20-1.0.26" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.10.0" apply false
 }
