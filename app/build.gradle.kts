603d2ff01266dad68bb6f706d2f61dde0fc88158
William Thi <140167371+WILLIAM-THI@users.noreply.github.com>
2026-09-06 01:43:12 +1000

    Updated plugins build.gradle.kts (project + app level)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("org.jetbrains.kotlin.plugin.parcelize") version "2.3.0"
+    id("androidx.navigation.safeargs.kotlin") version "2.10.0"
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.example.s8173367assignment2"
     compileSdk {
-        version = release(37)
+        version = release(37) {
+            minorApiLevel = 1
+        }
     }
 
     defaultConfig {
@@ -20,14 +25,19 @@ android {
 
     buildTypes {
         release {
-            optimization {
-                enable = false
-            }
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
@@ -36,7 +46,26 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
+    //implementation(libs.androidx.fragment.ktx)
+    //implementation(libs.androidx.lifecycle.livedata.ktx)
+    //implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    //implementation(libs.androidx.navigation.fragment.ktx)
+    //implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.material)
+    implementation(libs.material)
+    //implementation(libs.androidx.navigation.fragment)
+    //implementation(libs.androidx.navigation.ui)
+
+    // Coroutine dependancies
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
+
+    // Retrofit dependancies
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-moshi:3.0.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.5.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-
-    id("com.google.devtools.ksp") version "2.0.20-1.0.26" apply false
+    id("org.jetbrains.kotlin.plugin.parcelize") version "2.3.0" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.10.0" apply false
+    id("com.google.devtools.ksp") version "2.3.6" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.10.0")
+    }
 }
